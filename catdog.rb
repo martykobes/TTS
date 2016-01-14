7a70b8cf6d29ab39ff3106f2777da1442e3ac0c7
Marty Kowbel <mekobes44@yahoo.com>
2016-01-14 11:07:30 -0500

    first ruby push

---
+++ catdog.rb
@@ -0,0 +1,10 @@
+puts 'Please enter cat or dog'
+  name = gets.chomp.downcase
+  if name == 'cat'
+    puts 'Meow'
+  elsif name =='dog'
+    puts 'Woof'
+  else 
+    puts 'Hey enter cat or dog'
+    
+  end
---
+++ ex1.rb
@@ -0,0 +1,1 @@
+puts "Marty is awesome"
