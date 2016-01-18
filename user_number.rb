45ae99b95b8f5577e711943e54d03a6c997ffda7
Marty Kowbel <mekobes44@yahoo.com>
2016-01-18 12:34:20 -0500

    update user_number

--- user_number.rb
+++ user_number.rb
@@ -1,15 +1,7 @@
-=begin
-name = nil
-while name != 'done'
-    puts 'Enter a number or done to exit'
-    name = gets.chomp.downcase
-end
-=end
-
 total = 0
 user_input = nil
 while user_input != 'done'
-    print 'Enter a number to add to the total od done to exit and get your total  '
+    print 'Enter a number to add to the total or \'done\' to exit and get your total  '
   user_input = gets.chomp
   total = total + user_input.to_i
 end
