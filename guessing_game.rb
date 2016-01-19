738c3265d80ffbac4ad3fefa43c2decfb4c0a77b
Marty Kowbel <mekobes44@yahoo.com>
2016-01-19 08:19:54 -0500

    update guessinf game

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,10 +1,13 @@
-number = 5
+
 puts 'Enter a number between 0 amd 100'
+while true
   guess = gets.chomp.to_i
-if guess == number
+if guess == 7
     puts 'You got it right'
-elsif guess == number + 5 && number - 5
+  break
+elsif guess + 5 && guess - 5
     puts 'You are so close'
 else
     puts 'You are very cold, try again'
+end
 end
