e34f23b4773009ae19818048c8b11dbd42bf8116
Marty Kowbel <mekobes44@yahoo.com>
2016-01-19 08:30:04 -0500

    update guessing_game

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,13 +1,13 @@
-
+number = rand(1..100)
 puts 'Enter a number between 0 amd 100'
 while true
   guess = gets.chomp.to_i
-if guess == 7
+  if guess == number
     puts 'You got it right'
   break
 elsif guess + 5 && guess - 5
-    puts 'You are so close'
+    puts 'You are so close, try again'
 else
     puts 'You are very cold, try again'
-end
+  end
 end
